4eeadacbf199de1eb20c68a503ea43801331330c
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-09-05 13:50:38 +0800

    Add files via upload

--- implementations/dcgan/dcgan.py
+++ implementations/dcgan/dcgan.py
@@ -16,8 +16,10 @@ import torch
 
 os.makedirs("images", exist_ok=True)
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-imgdir','--img-dir',help='train image dir',default=r"C:\factory_data\2022-08-26\f_384_2min\crops_ori")
+#parser.add_argument('-loadweight','--load-weight',help='load weight or not',default="True")
+parser.add_argument('-imgdir','--img-dir',help='train image dir',default=r"/home/ali/YOLOV5/runs/detect/f_384_2min/crops_ori")
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
@@ -27,7 +29,8 @@ parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads 
 parser.add_argument("--latent_dim", type=int, default=400, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=200, help="interval between image sampling")
+parser.add_argument("--sample_interval_2", type=int, default=10, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
 
@@ -49,6 +52,7 @@ class Generator(nn.Module):
 
         self.init_size = opt.img_size // 4
         self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
+        
 
         self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
@@ -111,10 +115,12 @@ if cuda:
     generator.cuda()
     discriminator.cuda()
     adversarial_loss.cuda()
-
-# Initialize weights
-generator.apply(weights_init_normal)
-discriminator.apply(weights_init_normal)
+    
+TRAIN=True
+if TRAIN:
+    # Initialize weights
+    generator.apply(weights_init_normal)
+    discriminator.apply(weights_init_normal)
 
 # Configure data loader
 
@@ -162,72 +168,153 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 # ----------
 #  Training
 # ----------
-SAVE_MODEL_G_DIR = "./runs/train/"
-if not os.path.exists(SAVE_MODEL_G_DIR): os.makedirs(SAVE_MODEL_G_DIR)
-
-SAVE_MODEL_G_PATH = os.path.join(SAVE_MODEL_G_DIR,"g_net.pt")
-SAVE_MODEL_D_PATH = os.path.join(SAVE_MODEL_G_DIR,"d_net.pt")
-_lowest_loss = 10000
-for epoch in range(opt.n_epochs):
-    train_loss = 0
-    for i, (imgs, _) in enumerate(dataloader):
-
-        # Adversarial ground truths
-        valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
-        fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
-
-        # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
-
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        optimizer_G.zero_grad()
-
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-
-        # Generate a batch of images
-        gen_imgs = generator(z)
-
-        # Loss measures generator's ability to fool the discriminator
-        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Measure discriminator's ability to classify real from generated samples
-        real_loss = adversarial_loss(discriminator(real_imgs), valid)
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
-
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
-        loss = d_loss + g_loss
-        train_loss += loss.item()*imgs.size(0)
-    train_loss = train_loss/len(dataloader)
-    print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
-    print('_lowest_loss = {}'.format(_lowest_loss))
-    if train_loss < _lowest_loss:
-        _lowest_loss = train_loss
+if TRAIN:
+    SAVE_MODEL_G_DIR = "./runs/train/"
+    if not os.path.exists(SAVE_MODEL_G_DIR): os.makedirs(SAVE_MODEL_G_DIR)
+    
+    SAVE_MODEL_G_DIR = "./runs/train/"
+    SAVE_MODEL_G_PATH = os.path.join(SAVE_MODEL_G_DIR,"g_net.pt")
+    SAVE_MODEL_D_PATH = os.path.join(SAVE_MODEL_G_DIR,"d_net.pt")
+    #generator.load_state_dict(torch.load(SAVE_MODEL_G_PATH))
+    #discriminator.load_state_dict(torch.load(SAVE_MODEL_D_PATH))
+    if os.path.exists(SAVE_MODEL_G_PATH):
+        generator = torch.load(SAVE_MODEL_G_PATH)
+        discriminator = torch.load(SAVE_MODEL_D_PATH)
+        print('load weight G: {}'.format(SAVE_MODEL_G_PATH))
+        print('load weight D: {}'.format(SAVE_MODEL_D_PATH))
+    _lowest_loss = 10000
+    for epoch in range(opt.n_epochs):
+        train_loss = 0
+        for i, (imgs, _) in enumerate(dataloader):
+    
+            # Adversarial ground truths
+            valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+    
+            # Configure input
+            real_imgs = Variable(imgs.type(Tensor))
+    
+            # -----------------
+            #  Train Generator
+            # -----------------
+    
+            optimizer_G.zero_grad()
+    
+            # Sample noise as generator input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+    
+            # Generate a batch of images
+            gen_imgs = generator(z)
+    
+            # Loss measures generator's ability to fool the discriminator
+            g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+    
+            g_loss.backward()
+            optimizer_G.step()
+    
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+    
+            optimizer_D.zero_grad()
+    
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = adversarial_loss(discriminator(real_imgs), valid)
+            fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+    
+            d_loss.backward()
+            optimizer_D.step()
+    
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            )
+    
+            batches_done = epoch * len(dataloader) + i
+            if batches_done % opt.sample_interval == 0:
+                save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+           
+            loss = d_loss + g_loss
+            train_loss += loss.item()*imgs.size(0)
+        train_loss = train_loss/len(dataloader)
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+        print('_lowest_loss = {}'.format(_lowest_loss))
+        if train_loss < _lowest_loss:
+            _lowest_loss = train_loss
+            
+        print('Start save model !')
+        #torch.save(generator.state_dict(), SAVE_MODEL_G_PATH)
+        #torch.save(discriminator.state_dict(), SAVE_MODEL_D_PATH)
+        torch.save(generator, SAVE_MODEL_G_PATH)
+        torch.save(discriminator, SAVE_MODEL_D_PATH)
+        print('save model weights complete with loss : %.3f' %(train_loss))
         
-    print('Start save model !')
-    torch.save(generator.state_dict(), SAVE_MODEL_G_PATH)
-    torch.save(discriminator.state_dict(), SAVE_MODEL_D_PATH)
-    print('save model weights complete with loss : %.3f' %(train_loss))
+'''generate model unable to generate best image'''
+TEST=False
+if TEST:
+    os.makedirs("images_2",exist_ok=True)
+    SAVE_MODEL_G_DIR = "./runs/train/"
+    SAVE_MODEL_G_PATH = os.path.join(SAVE_MODEL_G_DIR,"g_net.pt")
+    SAVE_MODEL_D_PATH = os.path.join(SAVE_MODEL_G_DIR,"d_net.pt")
+    #generator.load_state_dict(torch.load(SAVE_MODEL_G_PATH))
+    #discriminator.load_state_dict(torch.load(SAVE_MODEL_D_PATH))
+    
+    generator = torch.load(SAVE_MODEL_G_PATH)
+    discriminator = torch.load(SAVE_MODEL_D_PATH)
+    
+    for epoch in range(opt.n_epochs):
+        train_loss = 0
+        with torch.no_grad():
+            for i, (imgs, _) in enumerate(dataloader):
+                
+                # Adversarial ground truths
+                valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+                fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+        
+                # Configure input
+                real_imgs = Variable(imgs.type(Tensor))
+                print('real_img : {}'.format(real_imgs.shape))
+                #real_imgs = real_imgs.view(-1,opt.latent_dim)
+                #print('real_img after view : {}'.format(real_imgs.shape))
+                # -----------------
+                #  Train Generator
+                # -----------------
+        
+                #optimizer_G.zero_grad()
+        
+                # Sample noise as generator input
+                z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+        
+                # Generate a batch of images
+                gen_imgs = generator(z)
+                #gen_imgs = generator(real_imgs)
+                
+                # Loss measures generator's ability to fool the discriminator
+                g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+        
+                #g_loss.backward()
+                #optimizer_G.step()
+                '''
+                # ---------------------
+                #  Train Discriminator
+                # ---------------------
+        
+                optimizer_D.zero_grad()
+        
+                # Measure discriminator's ability to classify real from generated samples
+                real_loss = adversarial_loss(discriminator(real_imgs), valid)
+                fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+                d_loss = (real_loss + fake_loss) / 2
+        
+                d_loss.backward()
+                optimizer_D.step()
+                '''
+                print(
+                    "[Epoch %d/%d] [Batch %d/%d] [G loss: %f]"
+                    % (epoch, opt.n_epochs, i, len(dataloader), g_loss.item())
+                )
+                
+                batches_done = epoch * len(dataloader) + i
+                if batches_done % opt.sample_interval_2 == 0:
+                    save_image(gen_imgs.data[:25], "images_2/%d.png" % batches_done, nrow=5, normalize=True)
