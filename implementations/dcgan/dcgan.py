132fd6593c9e51e3e86e85ab5c57627be84f613d
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-09-06 17:31:26 +0800

    Add files via upload

--- implementations/dcgan/dcgan.py
+++ implementations/dcgan/dcgan.py
@@ -20,17 +20,17 @@ os.makedirs("images", exist_ok=True)
 parser = argparse.ArgumentParser()
 parser.add_argument('-test','--test',type=bool,help='do test',default=False)
 parser.add_argument('-train','--train',type=bool,help='do train',default=True)
-parser.add_argument('-loadweight','--load-weight',type=bool,help='load weight or not',default=False)
+parser.add_argument('-loadweight','--load-weight',type=bool,help='load weight or not',default=True)
 parser.add_argument('-imgdir','--img-dir',help='train image dir',default=r"/home/ali/YOLOV5/runs/detect/f_384_2min/crops_ori")
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--test_batch_size", type=int, default=1, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=400, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=192, help="size of each image dimension")
+parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=200, help="interval between image sampling")
 parser.add_argument("--sample_interval_2", type=int, default=10, help="interval between image sampling")
@@ -126,7 +126,6 @@ if TRAIN:
     discriminator.apply(weights_init_normal)
 
 # Configure data loader
-
 import torchvision
 def load_data(args):
     size = (args.img_size,args.img_size)
@@ -137,7 +136,7 @@ def load_data(args):
                                                 #transforms.Scale(64),
                                                 transforms.CenterCrop(size),                                                 
                                                 transforms.ToTensor(),
-                                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
+                                                #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
                                                 ])
                                                 )
     data_loader = torch.utils.data.DataLoader(img_data, batch_size=args.batch_size,shuffle=True,drop_last=True)
@@ -153,7 +152,7 @@ def load_data_test(args):
                                                 #transforms.Scale(64),
                                                 transforms.CenterCrop(size),                                                 
                                                 transforms.ToTensor(),
-                                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
+                                                #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
                                                 ])
                                                 )
     data_loader = torch.utils.data.DataLoader(img_data, batch_size=args.test_batch_size,shuffle=False,drop_last=True)
@@ -255,7 +254,7 @@ if TRAIN:
     
             batches_done = epoch * len(dataloader) + i
             if batches_done % opt.sample_interval == 0:
-                save_image(gen_imgs.data[:opt.batch_size], "images/%d.png" % batches_done, nrow=4, normalize=True)
+                save_image(gen_imgs.data[:36], "images/%d.png" % batches_done, nrow=6, normalize=True)
            
             loss = d_loss + g_loss
             train_loss += loss.item()*imgs.size(0)
@@ -289,54 +288,23 @@ if TEST:
         train_loss = 0
         with torch.no_grad():
             for i, (imgs, _) in enumerate(dataloader_test):
-                
                 # Adversarial ground truths
                 valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
                 fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
-        
-                # Configure input
-                real_imgs = Variable(imgs.type(Tensor))
-                print('real_img : {}'.format(real_imgs.shape))
-                #real_imgs = real_imgs.view(-1,opt.latent_dim)
-                #print('real_img after view : {}'.format(real_imgs.shape))
                 # -----------------
-                #  Train Generator
+                #  Inference Generator
                 # -----------------
-        
-                #optimizer_G.zero_grad()
-        
                 # Sample noise as generator input
                 z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-        
                 # Generate a batch of images
                 gen_imgs = generator(z)
-                #gen_imgs = generator(real_imgs)
-                
                 # Loss measures generator's ability to fool the discriminator
                 g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-        
-                #g_loss.backward()
-                #optimizer_G.step()
-                '''
-                # ---------------------
-                #  Train Discriminator
-                # ---------------------
-        
-                optimizer_D.zero_grad()
-        
-                # Measure discriminator's ability to classify real from generated samples
-                real_loss = adversarial_loss(discriminator(real_imgs), valid)
-                fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-                d_loss = (real_loss + fake_loss) / 2
-        
-                d_loss.backward()
-                optimizer_D.step()
-                '''
+                
                 print(
                     "[Epoch %d/%d] [Batch %d/%d] [G loss: %f]"
                     % (epoch, opt.n_epochs, i, len(dataloader), g_loss.item())
                 )
-                
                 batches_done = epoch * len(dataloader) + i
                 #if batches_done % opt.sample_interval_2 == 0:
                 save_image(gen_imgs.data[:1], "images_2/%d.png" % batches_done, nrow=1, normalize=True)
