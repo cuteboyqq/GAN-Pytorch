cfc72869c8a2b4ad9dccd6ccc5c317531c09e135
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-10-02 12:57:18 +0800

    Add files via upload

--- implementations/discogan/datasets.py
+++ implementations/discogan/datasets.py
@@ -9,17 +9,26 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 class ImageDataset(Dataset):
-    def __init__(self, root, transforms_=None, mode='train'):
+    def __init__(self, root1,root2, transforms_=None, mode='train'):
         self.transform = transforms.Compose(transforms_)
 
-        self.files = sorted(glob.glob(os.path.join(root, mode) + '/*.*'))
+        #self.files = sorted(glob.glob(os.path.join(root, mode) + '/*.*'))
+        
+        self.files1 = sorted(glob.glob(root1 + '/*.*'))
+        self.files2 = sorted(glob.glob(root2 + '/*.*'))
 
     def __getitem__(self, index):
 
-        img = Image.open(self.files[index % len(self.files)])
-        w, h = img.size
-        img_A = img.crop((0, 0, w/2, h))
-        img_B = img.crop((w/2, 0, w, h))
+        #img = Image.open(self.files[index % len(self.files)])
+        img1 = Image.open(self.files1[index % len(self.files1)])
+        img2 = Image.open(self.files2[index % len(self.files2)])
+        #w, h = img.size
+        w, h = img1.size
+        #img_A = img.crop((0, 0, w/2, h))
+        #img_B = img.crop((w/2, 0, w, h))
+        
+        img_A = img1.crop((0, 0, w, h))
+        img_B = img2.crop((0, 0, w, h))
 
         if np.random.random() < 0.5:
             img_A = Image.fromarray(np.array(img_A)[:, ::-1, :], 'RGB')
@@ -31,4 +40,4 @@ class ImageDataset(Dataset):
         return {'A': img_A, 'B': img_B}
 
     def __len__(self):
-        return len(self.files)
+        return len(self.files1)
