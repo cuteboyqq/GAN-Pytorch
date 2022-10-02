46b604fbbd652ac8a45c05d24001da86668857ba
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-10-02 23:06:42 +0800

    Add files via upload

--- implementations/cyclegan/cyclegan.py
+++ implementations/cyclegan/cyclegan.py
@@ -37,7 +37,7 @@ def get_opts():
     parser.add_argument("--img_width", type=int, default=128, help="size of image width")
     parser.add_argument("--channels", type=int, default=3, help="number of image channels")
     parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving generator outputs")
-    parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between saving model checkpoints")
+    parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model checkpoints")
     parser.add_argument("--n_residual_blocks", type=int, default=9, help="number of residual blocks in generator")
     parser.add_argument("--lambda_cyc", type=float, default=10.0, help="cycle loss weight")
     parser.add_argument("--lambda_id", type=float, default=5.0, help="identity loss weight")
